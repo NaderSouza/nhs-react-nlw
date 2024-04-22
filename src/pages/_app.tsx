b0fc70559570e43e31768e1e88bef1f8adebbc55
NaderSouza <naderhadadsouza@gmail.com>
2024-04-22 11:07:10 -0300

    fix: add project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import type { AppProps } from "next/app";
+import MyApp from "./index"; //verificar isso
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
