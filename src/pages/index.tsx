ea3b0c41bea1db2c417156c65a044e665478e424
NaderSouza <naderhadadsouza@gmail.com>
2024-04-24 05:20:10 -0300

    fix: just commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+interface MyButtonProps {
+  text: string;
+}
+
+function MyButton(props: MyButtonProps) {
+  return (
+    <button className="button bg-orange-400 h-10 px-3 rounded font-medium">
+      {props.text}
+    </button>
+  );
+}
+
 export default function MyApp() {
-  return <h1>Nadin eh foda</h1>;
+  return (
+    <div className="flex gap-2">
+      <MyButton text="Clique aqui" />
+      <MyButton text="Clique 2" />
+      <MyButton text="Clique 3" />
+      <MyButton text="oi" />
+
+      <img />
+      <input />
+    </div>
+  );
 }
